324e6d7a54c09e189ad64ae711b89e5274ff3767
kasahun-welela <kasahunwelela1@gmail.com>
2025-11-11 14:47:09 +0300

    Add remote image patterns to Next.js configuration for external image sources

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,31 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+      {
+        protocol: 'https',
+        hostname:'picsum.photos'
+      }
+      ,
+      {
+        protocol: 'https',
+        hostname:'cdn.jsdelivr.net'
+      },
+      {
+        protocol: 'https',
+        hostname:'avatars.githubusercontent.com'
+      }
+    ],
+  },
 };
 
 export default nextConfig;
